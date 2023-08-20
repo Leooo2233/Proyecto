3f7c96deea7e59681057512fb9fea435f09115e6
agu$ <agustin.vigelal.06@gmail.com>
2023-08-19 21:38:24 -0300

    arreglé errores aberrantes y catastróficos en el estilo y la estructura de la pagina web

--- funciones.js
+++ funciones.js
@@ -1,3 +1,11 @@
 // document.write ("Hola bro ¿todo bien?")
 //Si bro, todo bien
 // me alegro bro
+
+//para que esto?
+seccionA = document.querySelector(".aside__contenedor")
+
+seccionA.style.display = "none"
+
+
+
