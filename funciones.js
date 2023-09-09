05f3bdb5e3dcd8ffd5226d0c128d377abcb2ad0b
Leonel Vazquez <leoseba747@gmail.com>
2023-09-09 20:58:52 -0300

    Hola

--- funciones.js
+++ funciones.js
@@ -1,15 +1,11 @@
 // document.write ("Hola bro ¿todo bien?")
 //Si bro, todo bien
 // me alegro bro
-<<<<<<< HEAD
 // y vos?
-=======
 
 //para que esto?
+//Ni idea che
 seccionA = document.querySelector(".aside__contenedor")
 
 seccionA.style.display = "none"
 
-
-
->>>>>>> 3f7c96deea7e59681057512fb9fea435f09115e6
